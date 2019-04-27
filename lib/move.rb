5087e740e26a347cfe6a10336a4e5664d56af350
Jason  Montes <jasone.montes@gmail.com>
2019-04-27 23:02:48 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def input_to_index(board)
+def input_to_index(user_input)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
